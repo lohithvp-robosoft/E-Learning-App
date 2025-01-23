6e6e194e4a6222ae546689eefa1c235944f47593
lohithvp-robosoft <lohith.vp@robosoftin.com>
2025-01-23 20:34:23 +0530

    Made UserName unique false

--- src/main/java/com/robosoft/elearning/modal/User.java
+++ src/main/java/com/robosoft/elearning/modal/User.java
@@ -16,13 +16,14 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = false)
     private String userName;
 
     private String profileImageUrl;
 
     @Column(nullable = false)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @ElementCollection(fetch = FetchType.EAGER)
     private List<Role> roles = new ArrayList<>();
